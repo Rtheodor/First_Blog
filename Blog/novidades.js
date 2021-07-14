2dfb5f99c0784454222f62e42e0b79009046b3d1
Rafael <rafaeldacunhatheodoro@gmail.com>
2021-07-14 18:30:42 -0300

    melhor que ontem

---
+++ Blog/novidades.js
@@ -0,0 +1,9 @@
+
+const nome = "Rafaelmaisumjavai";
+
+var sabores = "bolinhos de todos os tipos";
+
+function botoesmenu (){
+    alert("Funfa ou funfa!");
+    document.write("Ele é o " + nome + " come muitos " + sabores);
+}
